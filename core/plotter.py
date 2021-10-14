049534ab38a97b5febb56cce4c1ea70d74353bf3
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2021-10-14 20:06:20 +0200

    Improve spectro

--- core/plotter.py
+++ core/plotter.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import seaborn as sns
 import pandas as pd
 from .models import DataObject
-from scipy import signal
 
 
 class Plotter:
@@ -35,20 +33,20 @@ class Plotter:
         axes[2, 0].set(xlabel="Time (UTC)", ylabel="Velocity [$km \cdot s^{-1}$]")
         axes[2, 0].locator_params(nbins=6, axis="y")
 
-        f0, t0, Sxx0 = signal.spectrogram(do.data[0], 13)
-        f1, t1, Sxx1 = signal.spectrogram(do.data[1], 13)
-        f2, t2, Sxx2 = signal.spectrogram(do.data[2], 13)
+        s0, f0, t0, _ = plt.specgram(x=do.data[0], Fs=20)
+        s1, f1, t1, _ = plt.specgram(x=do.data[1], Fs=20)
+        s2, f2, t2, _ = plt.specgram(x=do.data[2], Fs=20)
 
         axes[0, 1].set_title("Vertical (Z) component")
-        axes[0, 1].pcolormesh(t0, f0, Sxx0, shading="gouraud")
+        axes[0, 1].pcolormesh(t0, f0, s0, shading="gouraud")
         axes[0, 1].set(xlabel="Time [sec]", ylabel="Frequency [Hz]")
 
-        axes[2, 1].set_title("Horizontal (N) component")
-        axes[1, 1].pcolormesh(t1, f1, Sxx1, shading="gouraud")
+        axes[1, 1].set_title("Horizontal (N) component")
+        axes[1, 1].pcolormesh(t1, f1, s1, shading="gouraud")
         axes[1, 1].set(xlabel="Time [sec]", ylabel="Frequency [Hz]")
 
         axes[2, 1].set_title("Horizontal (E) component")
-        axes[2, 1].pcolormesh(t2, f2, Sxx2, shading="gouraud")
+        axes[2, 1].pcolormesh(t2, f2, s2, shading="gouraud")
         axes[2, 1].set(xlabel="Time [sec]", ylabel="Frequency [Hz]")
 
         plt.subplots_adjust(top=0.88)
@@ -91,23 +89,23 @@ class Plotter:
         plt.savefig(f"out/{do.type}-{do.id}-series.png")
 
     def plot_spectrogram(self, do: DataObject):
-        f0, t0, Sxx0 = signal.spectrogram(do.data[0], 13)
-        f1, t1, Sxx1 = signal.spectrogram(do.data[1], 13)
-        f2, t2, Sxx2 = signal.spectrogram(do.data[2], 13)
+        s0, f0, t0, _ = plt.specgram(x=do.data[0], Fs=20)
+        s1, f1, t1, _ = plt.specgram(x=do.data[1], Fs=20)
+        s2, f2, t2, _ = plt.specgram(x=do.data[2], Fs=20)
 
         _, axes = plt.subplots(3, 1, figsize=(7, 6))
         plt.subplots_adjust(hspace=0.80)
 
         axes[0].set_title("Vertical (Z) component")
-        axes[0].pcolormesh(t0, f0, Sxx0, shading="gouraud")
+        axes[0].pcolormesh(t0, f0, s0, shading="gouraud")
         axes[0].set(xlabel="Time [sec]", ylabel="Frequency [Hz]")
 
         axes[2].set_title("Horizontal (N) component")
-        axes[1].pcolormesh(t1, f1, Sxx1, shading="gouraud")
+        axes[1].pcolormesh(t1, f1, s1, shading="gouraud")
         axes[1].set(xlabel="Time [sec]", ylabel="Frequency [Hz]")
 
         axes[2].set_title("Horizontal (E) component")
-        axes[2].pcolormesh(t2, f2, Sxx2, shading="gouraud")
+        axes[2].pcolormesh(t2, f2, s2, shading="gouraud")
         axes[2].set(xlabel="Time [sec]", ylabel="Frequency [Hz]")
 
         type = "Earthquake" if do.type == "EQ" else "Noise"
