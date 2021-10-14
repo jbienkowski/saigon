426801226db658973d0f83eeb1b4b7b2f7688f98
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2021-10-14 21:33:55 +0200

    WIP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from core.hd5_reader import Hd5Reader
 from core.plotter import Plotter
 
@@ -14,6 +15,10 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     """Script entry point"""
     logger.info("Bonjour!")
+
+    cfg = None
+    with open("./config.json", "r") as f:
+        cfg = json.load(f)
 
     hdf = Hd5Reader("data/LEN-DB.hdf5")
     eq_obc = hdf.get_random_object("EQ")
