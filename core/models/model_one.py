890fd9081924f0c77ac1000f486f4cf93e4f1e31
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2021-10-18 14:14:55 +0200

    Minor WIP

--- core/models/model_one.py
+++ core/models/model_one.py
@@ -10,11 +10,11 @@ class ModelOne(ModelBase):
     def define_model(self):
         self.model = Sequential()
         self.model.add(Dropout(0.2, seed=42, input_shape=(self.total_inputs,)))
-        self.model.add(Dense(128, activation="relu", name="m3_hidden1"))
+        self.model.add(Dense(128, activation="relu", name="hidden1"))
         self.model.add(Dropout(0.25, seed=42))
-        self.model.add(Dense(64, activation="relu", name="m3_hidden2"))
-        self.model.add(Dense(15, activation="relu", name="m3_hidden3"))
-        self.model.add(Dense(10, activation="softmax", name="m3_output"))
+        self.model.add(Dense(64, activation="relu", name="hidden2"))
+        self.model.add(Dense(15, activation="relu", name="hidden3"))
+        self.model.add(Dense(10, activation="softmax", name="output"))
         self.model.compile(
             optimizer="adam",
             loss="sparse_categorical_crossentropy",
